7faa8f4b6e242d863763b26bfc6f266cde120bc6
Janos Feher <feherjani96@gmail.com>
2018-10-14 02:17:56 +0200

    Layout enhancements

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :events
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'articles#index'
+  root to: redirect('/')
 end
