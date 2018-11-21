8188f6ddd4dafd115a0bd21d904adb61f1e64397
Janos Feher <feherjani96@gmail.com>
2018-11-21 17:09:13 +0100

    Add auth via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :photos
   resources :meals
   resources :categories
-  resources :users
   resources :pages
   resources :events
   resources :articles
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_192903) do
+ActiveRecord::Schema.define(version: 2018_11_21_145834) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -62,14 +62,15 @@ ActiveRecord::Schema.define(version: 2018_10_15_192903) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "auth_sch_id"
-    t.string "mail"
-    t.boolean "admin"
-    t.string "image"
-    t.text "introduction"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "meals", "categories"
