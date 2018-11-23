6dad19dbd73cb8b4dd769df2cedabdc142ccb7b3
Janos Feher <feherjani96@gmail.com>
2018-11-23 20:57:14 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+articles = Article.create([
+  {
+    title: 'Elso hír',
+    intro: 'Bevezető szöveg',
+    content: 'Lórum ipse az egyik akus smulális fóka. Szavagos oláp a srófban, majd szolon csillapodik. Bizgós mernás tárk, feke sükség, törös ötel nyújtja. Jubizmus: a dikarásnak taplósnak, bölőnek és komasosnak kell lennie. A csolált dikarást a régi nyadásoktól a tágos féleg cellájáig dugáznia kell! Az idén immár tizennegyedik főtlennel korított hekló a csernyes szatrum letesség jogoshoz figyelő cserszép és hulladt köződésök utbájára. Erre a zavatásra a száldás hörzs malom lórium faságán csernyes gisztés gyenség pület külés és a hörzs jetkeseinek széptéjében korított hekló.'
+  }
+])
+
+events = Event.create([
+  {
+    title: 'Event',
+    intro: 'Ez egy intro',
+    content: 'Well, the way they make shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and become television programs. Some don\'t, become nothing. '
+  }
+])
+
+finom_category = Category.create(
+    name: 'Finom'
+)
+
+meals = Meal.create([
+  {
+    name: 'Kaja',
+    toppings: 'semmi, valami, másvalami',
+    category: finom_category
+  }
+])
