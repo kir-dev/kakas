f34c5bb9cc9f26d9e1e10556bf6972d1d643acb7
Janos Feher <feherjani96@gmail.com>
2018-11-25 03:06:23 +0100

    Add image field to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def admin?
     current_user&.admin?
   end
@@ -6,5 +8,11 @@ class ApplicationController < ActionController::Base
 
   def require_admin
     redirect_to root_url unless current_user&.admin?
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, 
+      :password_confirmation, :current_password, :image, :image_cache, :remove_image) }
   end
 end
