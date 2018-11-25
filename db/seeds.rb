6efd040ddea5d342757da2c60699935712a6578e
Janos Feher <feherjani96@gmail.com>
2018-11-26 00:38:15 +0100

    Refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-articles = Article.create([
+Article.create([
   {
-    title: 'Elso hír',
+    title: 'Első hír',
     intro: 'Bevezető szöveg',
     content: 'Lórum ipse az egyik akus smulális fóka. Szavagos oláp a srófban, majd szolon csillapodik. Bizgós mernás tárk, feke sükség, törös ötel nyújtja. Jubizmus: a dikarásnak taplósnak, bölőnek és komasosnak kell lennie. A csolált dikarást a régi nyadásoktól a tágos féleg cellájáig dugáznia kell! Az idén immár tizennegyedik főtlennel korított hekló a csernyes szatrum letesség jogoshoz figyelő cserszép és hulladt köződésök utbájára. Erre a zavatásra a száldás hörzs malom lórium faságán csernyes gisztés gyenség pület külés és a hörzs jetkeseinek széptéjében korított hekló.'
   }
 ])
 
-events = Event.create([
+Event.create([
   {
-    title: 'Event',
-    intro: 'Ez egy intro',
+    title: 'Fontos esemény',
+    intro: 'Ez egy intro a fontos eseményhez. Mindenki olvassa el, nagyon fontos ám.',
     content: 'Well, the way they make shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and become television programs. Some don\'t, become nothing. '
   }
 ])
@@ -26,7 +26,7 @@ finom_category = Category.create(
     name: 'Finom'
 )
 
-meals = Meal.create([
+Meal.create([
   {
     name: 'Kaja',
     toppings: 'semmi, valami, másvalami',
@@ -34,9 +34,18 @@ meals = Meal.create([
   }
 ])
 
-user = User.new
-user.email = 'test@example.com'
-user.username = 'Teszt Elek'
-user.password = 'valid_password'
-user.password_confirmation = 'valid_password'
-user.save!
+users = User.create([
+  {
+    email: 'test@example.com',
+    username: 'Teszt Elek',
+    password: 'valid_password',
+    password_confirmation: 'valid_password'
+  },
+  {
+    email: 'admin@example.com',
+    username: 'Fontos Ember',
+    password: 'secure_password',
+    password_confirmation: 'secure_password',
+    admin: true
+  }
+])
