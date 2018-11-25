195cf070ce97962c528c56c87ff7c3a20518b499
Janos Feher <feherjani96@gmail.com>
2018-11-25 17:09:22 +0100

    Localize text to Hungarian

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Kakas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :'hu'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
