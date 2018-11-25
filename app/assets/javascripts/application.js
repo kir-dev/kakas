166291b394a47575ad9cd1c9bb50e431173040e6
Janos Feher <feherjani96@gmail.com>
2018-11-25 16:15:04 +0100

    Fix navbar dropdown behavior

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,23 +17,3 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
-$(document).ready(function() {
-  var windowsize = $(window).width();
-
-  if (windowsize >= 992) {
-    $(".btn-group").removeClass("dropright").addClass("dropleft")
-  } else {
-    $(".btn-group").removeClass("dropleft").addClass("dropright")
-  }
-})
-
-$(window).resize(function() {
-  var windowsize = $(window).width();
-
-  if (windowsize >= 992) {
-    $(".btn-group").removeClass("dropright").addClass("dropleft")
-  } else {
-    $(".btn-group").removeClass("dropleft").addClass("dropright")
-  }
-})
