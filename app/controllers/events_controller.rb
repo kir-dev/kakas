928330bdec23136d7b84069d69a6c38849038277
János Fehér <feherjani96@gmail.com>
2018-12-29 16:25:01 +0100

    Add event_date to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -54,6 +54,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :image, :intro, :content)
+      params.require(:event).permit(:title, :image, :intro, :content, :event_date)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_232242) do
+ActiveRecord::Schema.define(version: 2018_12_29_144829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_232242) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "event_date"
   end
 
   create_table "meals", force: :cascade do |t|
